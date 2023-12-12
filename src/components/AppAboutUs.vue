2edc997d35ddd43c2b3e0c9f05ba29ddf515b17e
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-12-12 16:41:58 +0100

    about us without props and style

--- src/components/AppAboutUs.vue
+++ src/components/AppAboutUs.vue
@@ -1,5 +1,38 @@
 <script></script>
 
-<template>wfqfq</template>
+<template>
+  <div class="row">
+    <div class="col-4">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <img src="/assets/about.png" class="my-about-img" />
+      </div>
+    </div>
+    <div class="col-8">
+      <div class="d-flex">
+        <img src="/assets/dog-icon.png" />
+        <span> About us</span>
+      </div>
+      <div class="d-flex">
+        <h2>Welcome DogMilo Pets</h2>
+      </div>
+      <div class="d-flex">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nisi
+        molestiae ducimus dicta obcaecati. Voluptatem sint blanditiis aliquid,
+        aperiam, quod quam quo minima ab nostrum consequatur architecto
+        explicabo cumque sunt?
+      </div>
+      <div class="d-flex">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nisi
+        molestiae ducimus dicta obcaecati. Voluptatem sint blanditiis aliquid,
+        aperiam, quod quam quo minima ab nostrum consequatur architecto
+        explicabo cumque sunt?
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.my-about-img {
+  width: 30rem;
+}
+</style>
