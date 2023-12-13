9d03959c5c1fcf9e815ef2fc24cab6daace11328
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-12-13 16:28:39 +0100

    newsletter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,9 +14,16 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <AppNewsletter />
-    </div>
+  <div class="container">
+    <AppNewsletter />
+  </div>
+  <div class="my-footer-container">
+    <div class="container">ciao</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.my-footer-container {
+  background-color: black;
+}
+</style>
--- src/components/AppNewsletter.vue
+++ src/components/AppNewsletter.vue
@@ -1,22 +1,24 @@
 <script></script>
 
 <template>
-  <div class="row">
+  <div class="row justify-content-center">
     <div class="my-news-box">
       <div class="col-7">
         <img src="/assets/news.png" class="my-dog-img" />
       </div>
       <div class="col-5">
-        <h3>Subscribe to Our Newsletter</h3>
-        <div class="my-input-container">
-          <input
-            type="text"
-            placeholder="Your Email Here"
-            aria-label="Email"
-            aria-describedby="button-addon2"
-            class="my-input"
-          />
-          <button class="my-btn" type="button" id="button-addon2">@</button>
+        <div class="d-flex flex-column my-email-container">
+          <h3>Subscribe to Our Newsletter</h3>
+          <div class="my-input-container">
+            <input
+              type="text"
+              placeholder="Your Email Here"
+              aria-label="Email"
+              aria-describedby="button-addon2"
+              class="my-input"
+            />
+            <button class="my-btn" type="button" id="button-addon2">@</button>
+          </div>
         </div>
       </div>
     </div>
@@ -31,6 +33,7 @@
   height: 15rem;
   position: relative;
   margin-top: 10rem;
+  width: 80%;
 }
 
 .my-dog-img {
@@ -46,15 +49,17 @@
 }
 
 .my-input {
-  padding: 1.2rem 8rem;
+  width: 100%;
+  height: 58px;
   text-align: start;
   border: none;
+  padding: 10px 20px;
 }
 
-h3{
-    color: white;
-    margin-top: 5rem;
-    margin-bottom: 1.3rem;
+h3 {
+  color: white;
+  margin-top: 5rem;
+  margin-bottom: 1.3rem;
 }
 
 .my-btn {
@@ -64,8 +69,9 @@ h3{
   color: white;
   font-weight: bold;
   font-size: 1.5rem;
-  padding: 0.5rem 1.2rem;
-  top: 0.3rem;
-  right: 6.8rem;
+  width: 52px;
+  height: 52px;
+  right: 3px;
+  top: 3px;
 }
 </style>
